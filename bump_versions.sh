8801e048d3ff65890695de6a6f90a7ff7aef22f2
Filipe Santos <filipe.santos@piiano.com>
2025-01-06 11:02:39 +1300

    fix: version bump sed args

--- bump_versions.sh
+++ bump_versions.sh
@@ -25,11 +25,11 @@ OLD_VAULT_VERSION=$(helm show values --jsonpath '{.image.tag}' ${DIR})
 sed --in-place -e "s|$LINE \`$OLD_VAULT_VERSION\`|$LINE \`$NEW_VAULT_VERSION\`|g" ${README}
 
 # replace image tag in readme
-sed --in-place '' -e "s|\`$OLD_VAULT_VERSION\`|\`$NEW_VAULT_VERSION\`|g" ${README}
-sed --in-place '' -e "s|piiano/pvault-cli:$OLD_VAULT_VERSION\"|piiano/pvault-cli:$NEW_VAULT_VERSION\"|g" ${README}
+sed --in-place -e "s|\`$OLD_VAULT_VERSION\`|\`$NEW_VAULT_VERSION\`|g" ${README}
+sed --in-place -e "s|piiano/pvault-cli:$OLD_VAULT_VERSION\"|piiano/pvault-cli:$NEW_VAULT_VERSION\"|g" ${README}
 
 # replace tag in vaules.yaml
-sed --in-place '' -e "s|tag: $OLD_VAULT_VERSION|tag: $NEW_VAULT_VERSION|g" ${VALUES}
+sed --in-place -e "s|tag: $OLD_VAULT_VERSION|tag: $NEW_VAULT_VERSION|g" ${VALUES}
 
 # find and generate new helm chart version
 # use ^ to find the version starting at the beginning of a line to not confuse with psql version
@@ -37,5 +37,5 @@ CUR_HELM_VER=$(grep '^version' ${CHART} | sed 's/version: //g' | sed 's/"//g')
 NEW_HELM_VER=$(npx --yes semver -i "${BUMP_CHART_TYPE}" "${CUR_HELM_VER}")
 
 # bump main chart version and appversion
-sed --in-place '' -E "s|version: [0-9]+\.[0-9]+\.[0-9]+|version: ${NEW_HELM_VER}|g" ${CHART}
-sed --in-place '' -E "s|appVersion: [0-9]+\.[0-9]+\.[0-9]+|appVersion: ${NEW_VAULT_VERSION}|g" ${CHART}
+sed --in-place -E "s|version: [0-9]+\.[0-9]+\.[0-9]+|version: ${NEW_HELM_VER}|g" ${CHART}
+sed --in-place -E "s|appVersion: [0-9]+\.[0-9]+\.[0-9]+|appVersion: ${NEW_VAULT_VERSION}|g" ${CHART}
